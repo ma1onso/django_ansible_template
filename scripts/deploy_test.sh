df373da9d9e4a43b41ea2dacabf638c135c21867
Alonso Enrique <alons45@gmail.com>
2018-11-01 13:26:58 -0500

    improvement multi staging

--- scripts/deploy_test.sh
+++ scripts/deploy_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./ansible/test/test_deploy.yml --private-key=ssh_keys/test/id_rsa -K -u deployer -i ./ansible/test/hosts -vvv
+ansible-playbook ./ansible/deploy.yml --private-key=ssh_keys/test/id_rsa -K -u deployer -i ./ansible/test/hosts -vvv
